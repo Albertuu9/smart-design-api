6fcb6486bc8bffba5b6d0c4e77e365533b694e6e
Alberto Gonzalez <>
2021-01-10 03:01:02 +0100

    Se crea un archivo para almacenar las variables de entorno

--- src/app/settings/settings.js
+++ src/app/settings/settings.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 
 const settings = {
-    port: '7777',
-    dbUri: 'mongodb+srv://albertuu9:danilinho5@smartdesign.eqhqt.mongodb.net/smartdesign?retryWrites=true&w=majority',
-    secret: 'agfinformatique'
+    port: '3000',
+    dbUri: 'mongodb+srv://albertuu9:'+process.env.MONGO_PASSWORD+'@smartdesign.eqhqt.mongodb.net/smartdesign?retryWrites=true&w=majority',
+    secret: process.env.SECRET
 }
 
 module.exports = { settings }

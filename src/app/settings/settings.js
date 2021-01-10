2c413d17a3abdbf632007234c74deae054eb0726
Alberto González <55255601+Albertuu9@users.noreply.github.com>
2021-01-10 02:35:41 +0100

    Update settings.js

--- src/app/settings/settings.js
+++ src/app/settings/settings.js
@@ -1,7 +1,7 @@
 
 const settings = {
     port: '7777',
-    dbUri: 'mongodb+srv://albertuu9:danilinho5@smartdesign.eqhqt.mongodb.net/smartdesign?retryWrites=true&w=majority',
+    dbUri: 'mongodb+srv://smartdesign.eqhqt.mongodb.net/smartdesign?retryWrites=true&w=majority',
     secret: 'agfinformatique'
 }
 
